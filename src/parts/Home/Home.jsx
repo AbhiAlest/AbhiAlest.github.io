f799fbecfc5678a436064fb5c3c40fcde836aef6
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-04-21 19:32:43 -0500

    Create Home.jsx

---
+++ src/parts/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <div className="main-content">
+      <div className="header">
+        <h1>AbhiAlest</h1>
+        <h2>Diligent Front-End Developer</h2>
+      </div>
+      <div className="image-container">
+        <div className="blue-square"></div>
+        <img src="/path/to/photo" alt="AbhiAlest" />
+      </div>
+    </div>
+  );
+}
+
+export default MainContent;
