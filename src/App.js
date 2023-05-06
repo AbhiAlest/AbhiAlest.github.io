0dc00825dadb010f23a3c6cbcc4bad7ab71dece6
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-05 20:37:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Welcome to my website</h1>
+      <p>Here is some content for my website.</p>
+    </div>
+  );
+}
+
+export default App;
 
