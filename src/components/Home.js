5e55dfbfca7004a0335b2dd21e0ae969b3566622
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-06 18:36:11 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
+import profilePic from "../assets/profile-pic.jpg";
 
 function Home() {
   return (
     <div className="container">
-      <h1 className="display-4">AbhiAlest</h1>
-      <p className="lead">Diligent Front-End Dev</p>
+      <div className="row">
+        <div className="col-md-6 order-md-2">
+          <img
+            src={profilePic}
+            alt="Your Name"
+            className="img-fluid rounded"
+          />
+        </div>
+        <div className="col-md-6 order-md-1">
+          <h1 className="display-4">Your Name</h1>
+          <p className="lead">Short description about yourself</p>
+        </div>
+      </div>
     </div>
   );
 }
