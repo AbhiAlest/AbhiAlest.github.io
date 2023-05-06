63614d2c02ee6cd85e8a60571d2d79c35eaf009a
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-06 18:12:40 -0500

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="container">
+      <h1 className="display-4">AbhiAlest</h1>
+      <p className="lead">Diligent Front-End Dev</p>
+    </div>
+  );
+}
+
+export default Home;
