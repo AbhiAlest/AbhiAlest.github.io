8f5e9bc16b3bf18253fd2c33e1b18bd9ea93f88f
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-05 20:35:30 -0500

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+
+    const root = document.documentElement;
+    if (isDarkMode) {
+      root.style.setProperty("--background-color", "#fff");
+      root.style.setProperty("--text-color", "#333");
+    } else {
+      root.style.setProperty("--background-color", "#333");
+      root.style.setProperty("--text-color", "#fff");
+    }
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar__left">
+        <Link to="/">
+          <img src="/logo.png" alt="Logo" className="navbar__logo" />
+        </Link>
+        <div className="navbar__gap" style={{ width: "500px" }} />
+        <Link to="/">Home</Link>
+        <Link to="/about">About Me</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/blog">Blog</Link>
+      </div>
+      <div className="navbar__right">
+        <button onClick={toggleDarkMode}>
+          {isDarkMode ? "Light mode" : "Dark mode"}
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
