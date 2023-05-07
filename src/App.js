1308f0dadf04040be5038b09bdaca5238d82077b
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-06 22:20:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Blog from "./components/Blog";
 import Profile from "./components/Profile";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/profile" component={Profile} />
-      </Switch>
-    </BrowserRouter>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/blog" component={Blog} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
