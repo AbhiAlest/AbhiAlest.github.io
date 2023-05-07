c1ddfde2513508d6510bf931b6644edc5e6816fd
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-05-06 19:22:36 -0500

    Create Profile.js

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div className="container">
+      <h1>Profile</h1>
+      <p>
+        This is a longer description.
+      </p>
+    </div>
+  );
+}
+
+export default Profile;
